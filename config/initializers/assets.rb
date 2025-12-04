9ce107cc1b5626970c47bc4d7a731be51d4e3c8a
[egoruska85] <[egorarbuzov@gmail.com]>
2025-12-05 00:04:24 +0300

    ttt

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
-#Rails.application.config.assets.precompile += %w( sllde.css )
-#Rails.application.config.assets.precompile += %w( slide.js )
+Rails.application.config.assets.precompile += %w( sllde.css )
+Rails.application.config.assets.precompile += %w( slide.js )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
